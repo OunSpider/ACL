18288c5d48632de68801e37b79cb4f1041e2f051
OUN RUN <ounrun.github@gmail.com>
2024-05-30 00:19:54 +0700

    first commit:

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is: " + name);
+    }
+}
